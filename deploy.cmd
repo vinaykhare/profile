f55b19ebcaee509b1bd18b81b8fa1544a473a73d
Vinay Kumar Khare <vinaykhare@live.in>
2025-01-05 01:41:11 +0530

    updated deployment command

--- deploy.cmd
+++ deploy.cmd
@@ -1,11 +1,10 @@
-copy google6b5666910c947583.html build/web
 cd build/web 
 
 git init
 git add .
 git commit -m "Deploying changes"
 git branch -M main
-git remote add origin https://vinaykhare@github.com/vinaykhare/profile.git
+git remote add origin https://vinaykhare@github.com/vinaykhare/vinaykhare.github.io.git
 git push -u -f origin main
 
 @echo "✅ Finished deploy:"
